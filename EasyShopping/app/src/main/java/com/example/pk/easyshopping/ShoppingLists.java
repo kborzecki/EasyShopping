9ceaf618ae373a89fcf5205c5d49205c53808a7e
Lagger96 <franek616@gmail.com>
2017-06-15 22:00:35 +0200

    added RecipesList activity

--- EasyShopping/app/src/main/java/com/example/pk/easyshopping/ShoppingLists.java
+++ EasyShopping/app/src/main/java/com/example/pk/easyshopping/ShoppingLists.java
@@ -1,7 +1,12 @@
 package com.example.pk.easyshopping;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ShoppingLists extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class ShoppingLists extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_lists);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.shopping_lists_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+        if (menuItemThatWasSelected == R.id.action_recipes)
+        {
+            Context context = ShoppingLists.this;
+            Intent myIntent = new Intent(context, RecipesList.class);
+            context.startActivity(myIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
